fa3c448a4d20801634508fe37e844a070ec5f53b
yasmine Bebars <yasminebeb@hotmail.com>
2025-08-31 14:56:14 +0300

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+     <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
