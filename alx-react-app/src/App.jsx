bdbb5e310fbe5e6292b85fb53435ca956ac26702
yasmine Bebars <yasminebeb@hotmail.com>
2025-09-01 10:19:02 +0300

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
  import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -20,6 +21,7 @@ function App() {
        <WelcomeMessage />
         <Header />
         <MainContent />
+         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <div>
             <h1>User Profile</h1>
             {/* UserProfile with props */}
